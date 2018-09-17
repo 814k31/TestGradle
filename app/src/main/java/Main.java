7df7cfc21a5ad77721878d8631119c4446367995
Blake Lovelace <lovelace.b@hotmail.com>
2018-09-18 00:01:58 +0800

    Initial commit with one deep gradle project

---
+++ app/src/main/java/Main.java
@@ -0,0 +1,10 @@
+import com.blake.onedeep.OneDeep;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+		OneDeep o = new OneDeep();
+
+		System.out.println(o);
+	}
+}
---
+++ oneDeep/src/main/java/com/blake/onedeep/OneDeep.java
@@ -0,0 +1,10 @@
+package com.blake.onedeep;
+
+public class OneDeep {
+	public OneDeep() {}
+
+	@Override
+	public String toString() {
+		return "OneDeep";
+	}
+}
